85d13e911731779eeb778c461be1790f7427edc0
Rauno Viskus <rauno56@gmail.com>
2016-10-23 10:06:53 +0300

    delete trailing spaces

--- R/get-clean-data.R
+++ R/get-clean-data.R
@@ -4,38 +4,38 @@
 source("R/utils.r")
 
 generateDb = function(path = NULL) {
-    
+
     # Function for generating database from raw data files
-    # 
+    #
     # Args:
     #  path :  path to raw data folder
-    
-    # 
+
+    #
     message("Message: working directory is expected to contain folder /raw-data. Parameter 'path' should lead to /raw-data")
-    
+
     wd = getwd()
     path = paste0(wd,path)
-    if(!all(c("NAV","maht","indexfunds") %in% list.files(path))) stop("'path' must direct to /raw-data 
+    if(!all(c("NAV","maht","indexfunds") %in% list.files(path))) stop("'path' must direct to /raw-data
                                                          folder with subdirectories 'NAV' and 'maht'")
     # print("siin 1")
     navFiles  = list.files(paste0(path,"/NAV"), full.names = T)
     # print("siin 2")
-    mahtFiles = list.files(paste0(path,"/maht"), full.names = T) 
-    
+    mahtFiles = list.files(paste0(path,"/maht"), full.names = T)
+
     # read nav files
     nav = list()
     for(i in navFiles) nav[[i]] <- readFile(i)
     nav = data.table::rbindlist(nav)
-    
-    # read maht 
+
+    # read maht
     maht = list()
     for(j in mahtFiles) maht[[j]] <- readFile(j)
     # lapply(maht,head)
     maht = data.table::rbindlist(maht)
-    
+
     # merged data
     df = merge(nav, maht, by = c("Kuupäev", "Fond", "Lühinimi", "ISIN"))
-    
+
     df$time   = parseDate(df$Kuupäev)
     df$nav    = parseFloat(df$NAV)
     df$volume = df$Maht
@@ -45,34 +45,34 @@ generateDb = function(path = NULL) {
     # make list of fonds
     fonds = list()
     for(i in levels(df$ISIN)) fonds[[i]] = dplyr::filter(df, ISIN == i)
-    
+
     fonds = lapply(fonds, calcStats)
     message("Message: deleted rows where volume == 0")
-    
+
 
     fonds
-} 
+}
 
 getIndexFundsData = function(path = NULL){
-    
+
     message("Message: index fund raw data files must be named after ISIN codes.")
     message("Message: looking index fund data from ../idexfunds")
-    
+
     wd = getwd()
     path = paste0(wd,path,"/indexfunds")
-    
+
     indexFiles = list.files(path, full.names = T)
     indexFunds = list()
     for(i in indexFiles){
-        
+
         # print(i)
         tmpdf = data.table::fread(i, skip = 1)
         isin  = stringr::str_split(basename(i),pattern = "\\.")[[1]][1]
         tmpdf$isin = isin
         tmpdf$time = parseDoubleQuotesDates(tmpdf$Date)
         tmpdf$nav  = parseDoubleQuotesNumeric(tmpdf$NAV)
-        tmpdf$fond = NA 
-        
+        tmpdf$fond = NA
+
         # BAD bu does the work!
         tmpdf$fond[tmpdf$isin == "IE0009591805"] = "Euro Investment Grade Bond Index Fund"
         tmpdf$fond[tmpdf$isin == "IE00B03HCZ61"] = "Global Stock Index Fund"
